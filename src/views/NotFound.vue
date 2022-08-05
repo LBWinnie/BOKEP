f6381a5ff3cb9638191b3b8d43d90be5410e35a5
LBWinnie <206589036@qq.com>
2022-08-05 22:56:36 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>404</div>
+    <div><router-link to="/">返回首页</router-link></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
