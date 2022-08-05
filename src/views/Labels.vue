21edcd5b18c3c3609c9262c3eaca1ff8845a768d
LBWinnie <206589036@qq.com>
2022-08-06 01:05:58 +0800

    创建<Layout />

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,28 +1,9 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      <p>Labels</p>
-    </div>
-    <Nav />
-  </div>
+  <Layout>Labels</Layout>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Labels from "@/views/Labels.vue";
-
-export default Vue.extend({});
+export default {
+  name: "Labels",
+};
 </script>
-
-<style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column; //上下布局
-  height: 100vh;
-}
-.content {
-  overflow: auto;
-  flex-grow: 1; //使content尽量占据所有高度
-}
-</style>
