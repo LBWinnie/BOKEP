4434f2c75e76ad988c99cb7e9f3ecb1e38eff40a
LBWinnie <206589036@qq.com>
2022-08-05 21:33:14 +0800

    创建三个页面(记账&标签&统计)

---
+++ src/views/Labels.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Labels</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Labels from "@/views/Labels.vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+Statistics
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+import Statistics from '@/views/Statistics.vue';
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
